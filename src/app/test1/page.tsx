68d12abf518fdf25a46c4899c0d4e215182e62ba
FearlessSlug <134693517+FearlessSlug@users.noreply.github.com>
2023-05-26 13:13:54 +0300

    Update page.tsx

--- src/app/test1/page.tsx
+++ src/app/test1/page.tsx
@@ -6,16 +6,6 @@ export default async function Page() {
   return (
     <div>
       <h1 className="text-xl font-bold">Client component - test 1</h1>
-      <Link className="underline" href={{ query: { test: "test123" } }}>
-        Test 1
-      </Link>
-      <br />
-      <Link
-        className="underline"
-        href={{ pathname: "/test3", query: { test: "test123" } }}
-      >
-        Test 2
-      </Link>
     </div>
   );
 }
