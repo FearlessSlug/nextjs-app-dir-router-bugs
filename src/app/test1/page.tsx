cb967720b477c55597543e995982f0689a84cf29
FearlessSlug <134693517+FearlessSlug@users.noreply.github.com>
2023-05-30 13:20:18 +0300

    Fix the bug

--- src/app/test1/page.tsx
+++ src/app/test1/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import Link from "next/link";
-
-export default async function Page() {
+export default function Page() {
   return (
     <div>
       <h1 className="text-xl font-bold">Client component - test 1</h1>
